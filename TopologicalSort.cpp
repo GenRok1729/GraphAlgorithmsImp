ac2333a8c9ea17b05b566caabef9892f8d9c474a
GenRok1729 <71984114+GenRok1729@users.noreply.github.com>
2021-01-26 14:16:08 +0530

    Update TopologicalSort.cpp

--- TopologicalSort.cpp
+++ TopologicalSort.cpp
@@ -1,4 +1,10 @@
 //For smallest toposort you can use prioirity queue instead of q
+/**
+Kahn's algorithm is used to perform a topological sort on a directed acyclic graph with time complexity of O ( V + E )
+– where V is the number of vertices and E is the number of edges in the graph.
+
+
+**/
 
 bool canFinish(int n, vector<vector<int>>& vect) {
         
@@ -44,6 +50,20 @@ bool canFinish(int n, vector<vector<int>>& vect) {
     }
     
  //solution - https://leetcode.com/submissions/detail/349499280/
+
+//Topological sorting for Directed Acyclic Graph (DAG) is a linear ordering of vertices such that for every directed edge u v, vertex u comes before v in the 
+//ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
+
+//O(V). The extra space is needed for the vector.
+
+/**
+Note: Here, we can also use vector instead of the stack. If the vector is used then print the elements in reverse order to get the topological sorting.
+
+Applications: 
+Topological Sorting is mainly used for scheduling jobs from the given dependencies among jobs. In computer science, applications of this type arise 
+in instruction scheduling, ordering of formula cell evaluation when recomputing formula values in spreadsheets, logic synthesis, determining the order of
+compilation tasks to perform in make files, data serialization, and resolving symbol dependencies in linkers [2].
+**/
     
  //Problem 1: https://www.spoj.com/problems/TOPOSORT/
 //Solution 1: http://p.ip.fi/RTRG
