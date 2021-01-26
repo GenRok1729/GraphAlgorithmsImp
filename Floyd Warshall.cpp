6fcf2dda57f412512d877454d5f86fb064c1c385
GenRok1729 <71984114+GenRok1729@users.noreply.github.com>
2021-01-26 13:33:34 +0530

    Update Floyd Warshall.cpp

--- Floyd Warshall.cpp
+++ Floyd Warshall.cpp
@@ -1,5 +1,9 @@
 int dist[N][N];
 
+
+The Floyd Warshall Algorithm is for solving the All Pairs Shortest Path problem. The problem is to find shortest distances between every pair of vertices in a given edge weighted directed Graph
+
+
 void FloydWarshall()
 {
 	for(int k=1;k<=n;k++)
