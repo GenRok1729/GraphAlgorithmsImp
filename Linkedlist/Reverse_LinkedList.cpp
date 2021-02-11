9ce949d3c47a945f6a791e64b12f1b73f8344420
GenRok1729 <71984114+GenRok1729@users.noreply.github.com>
2021-02-11 15:29:25 +0530

    Update Reverse_LinkedList.cpp

--- Linkedlist/Reverse_LinkedList.cpp
+++ Linkedlist/Reverse_LinkedList.cpp
@@ -19,9 +19,30 @@ ListNode* reverseList(ListNode* head) {
         
         
         return q;
+    }
+
+
+==================================================================================================================================================================
         
+ListNode* reverseList(ListNode* head) {
+     
         
+        ListNode* p = NULL;
+        ListNode* q = head;
+        
+        while(q != NULL)
+        {
+            ListNode* temp = q->next;
+            q->next = p;
+            p = q;//for next onr this is the previous node 
+            q = temp;//This is the new node 
+            
+        }
+        
+        return p;//At lat this will be in the last node as its q willl be NULL
         
         
     }
+
+=================================================================================================================================================================
     
