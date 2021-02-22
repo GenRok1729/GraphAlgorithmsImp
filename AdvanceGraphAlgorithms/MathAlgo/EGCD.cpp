6a233c1fbde73399cb249d6e828e51e31c52a123
GenRok1729 <71984114+GenRok1729@users.noreply.github.com>
2021-02-22 14:53:04 +0530

    Update EGCD.cpp

--- AdvanceGraphAlgorithms/MathAlgo/EGCD.cpp
+++ AdvanceGraphAlgorithms/MathAlgo/EGCD.cpp
@@ -4,6 +4,12 @@ int gcd (int a, int b) {
     else
         return gcd (b, a % b);
 }
+
+//https://leetcode.com/problems/greatest-common-divisor-of-strings/
+//https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/ learning gcd of a number and 0 is number itself, and gcd of a number and 1 is 1
+//https://leetcode.com/problems/water-and-jug-problem/(Pending)
+//https://leetcode.com/problems/nth-magical-number/(Excellent Qyestion Must Do)
+
 ______________________________________________________________________________________________________________________________________________________________
 
 int gcd (int a, int b) {
